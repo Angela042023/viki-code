d280068b331b77e68a1de47750f01fdf093d5315
angela042023 <luzg75035@gmail.com>
2024-01-30 11:44:42 -0500

    creacion de paginas para visualizacion de videos

---
+++ script.js
@@ -0,0 +1,13 @@
+var swiper=new swiper(".myswiper",{
+
+    slidesperView:1,
+    spacaBetween:30,
+    grabCursor:true,
+    loop:true,
+    breakpoints: {
+        991:{
+            slidesperView:4
+        }
+    }
+
+})
